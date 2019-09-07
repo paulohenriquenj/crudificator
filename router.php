9d3ce335b4d559385b217b32fb95c412bdd5f937
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 16:49:16 -0300

    Add router to tableinfo

--- router.php
+++ router.php
@@ -1,5 +1,8 @@
 <?php
 
+use Crudificator\controller;
+use Crudificator\controller\crudificatorController;
+
 Flight::set('flight.views.path', __DIR__.'/views');
 
 
@@ -20,4 +23,23 @@ Flight::route('GET /config/database', function () {
     }
 
     Flight::halt(500, 'Form não encontrado.');
+});
+
+
+Flight::route('POST /database/tableInfo', function () {
+
+    $crudController = new crudificatorController();
+
+    
+    $crudController->getTableInfo([
+        'userName' => flight::request()->data['user'],
+        'password' => flight::request()->data['password'],
+        'host' => flight::request()->data['host'],
+        'database' => flight::request()->data['database'],
+        'db_type' => Flight::request()->data['db_type'],
+        'table' => Flight::request()->data['tabl'],
+    ]);
+
+
+    // Flight::halt(500, 'Form não encontrado.');
 });
