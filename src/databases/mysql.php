eb0476368db663d512e0cd6ade015a0f37b694fb
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 15:47:14 -0300

    add mysql class handle

--- src/databases/mysql.php
+++ src/databases/mysql.php
@@ -6,7 +6,17 @@ class mysql{
 
     public function connect($config)
     {
+        try {
+            $pdo = new PDO($this->parseConfig($config), $config['userName'], $config['password']);
+            return $pdo;
+        } catch (PDOException $e) {
+            echo 'Can not connect to database.';
+        }
+    }
 
+    public function parseConfig(array $config)
+    {
+        return 'mysql:' . $config['host'] . ';dbname='.$config['database'];
     }
 
 
