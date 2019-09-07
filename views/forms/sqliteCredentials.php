ce8a40a0cc260e5af316104f500d500f54c9ceea
Simonarde Lima <simonarde@gmail.com>
2019-09-07 15:35:16 -0300

    add form to configure sqlite credentials

---
+++ views/forms/sqliteCredentials.php
@@ -0,0 +1,18 @@
+<div class="row">
+    <div class="col-md-8 order-md-1">
+        <h4 class="mb-3">Credenciais</h4>
+        <form action="" method="POST" class="needs-validation" novalidate autocomplete="off">
+            <div class="form-group row">
+                <label for="path" class="col-sm-2 col-form-label">Path</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="path" placeholder="/meu/projeto/database.sqlite">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-success">Conectar</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
