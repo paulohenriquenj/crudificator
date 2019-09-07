0489d91983fe66c54860c2d734552428bb259f63
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 14:52:02 -0300

    Passing databases info

--- router.php
+++ router.php
@@ -3,8 +3,8 @@
 Flight::set('flight.views.path', __DIR__.'/views');
 
 
-Flight::route('/', function () {
-    Flight::render('selectDataBaseType', array('database' => 'Mysql'), 'content');
+Flight::route('/', function () use($supportedDataBases){
+    Flight::render('selectDataBaseType', ['supportedDataBases' => $supportedDataBases], 'content');
     Flight::render('layout');
 });
 
