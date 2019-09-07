ad9b462e681b6e74851ddc3edcaf2ca09a91d92a
Simonarde Lima <simonarde@gmail.com>
2019-09-07 15:32:48 -0300

    add table and adjusting layout

--- views/forms/mysqlCredentials.php
+++ views/forms/mysqlCredentials.php
@@ -1,58 +1,40 @@
 <div class="row">
     <div class="col-md-8 order-md-1">
         <h4 class="mb-3">Credenciais</h4>
-        <form class="needs-validation" novalidate autocomplete="off">
-            <div class="row">
-                <div class="col-md-6 mb-3">
-                    <label for="username">Usuário</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-fw fa-user"></i></span>
-                        </div>
-                        <input type="text" class="form-control" id="username" placeholder="Usuário" required style="border-radius: 3px">
-                        <div class="invalid-feedback" style="width: 100%;">
-                            O Usuário é obrigatório
-                        </div>
-                    </div>
-                </div>
-                <div class="col-md-6 mb-3">
-                    <label for="password">Senha</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-fw fa-eye-slash"></i></span>
-                        </div>
-                        <input type="password" class="form-control" id="password" placeholder="Senha" required style="border-radius: 3px">
-                        <div class="invalid-feedback" style="width: 100%;">
-                            A senha é obrigatória
-                        </div>
-                    </div>
+        <form action="" method="POST" class="needs-validation" novalidate autocomplete="off">
+            <div class="form-group row">
+                <label for="host" class="col-sm-2 col-form-label">Host</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="host" placeholder="Ex.: 192.168.0.0">
                 </div>
             </div>
-            
-            <div class="row">
-                <div class="col-md-6 mb-3">
-                    <label for="host">Host</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-fw fa-link"></i></span>
-                        </div>
-                        <input type="text" class="form-control" id="host" placeholder="Host" required style="border-radius: 3px">
-                        <div class="invalid-feedback" style="width: 100%;">
-                            O Host é obrigatório
-                        </div>
-                    </div>
+            <div class="form-group row">
+                <label for="usuario" class="col-sm-2 col-form-label">Usuário</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="usuario" placeholder="Usuário">
                 </div>
-                <div class="col-md-6 mb-3">
-                    <label for="database">Banco</label>
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text"><i class="fa fa-fw fa-server"></i></span>
-                        </div>
-                        <input type="text" class="form-control" id="database" placeholder="Banco" required style="border-radius: 3px">
-                        <div class="invalid-feedback" style="width: 100%;">
-                            O banco é obrigatório
-                        </div>
-                    </div>
+            </div>
+            <div class="form-group row">
+                <label for="password" class="col-sm-2 col-form-label">Senha</label>
+                <div class="col-sm-10">
+                    <input type="password" class="form-control" id="password" placeholder="Senha">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="database" class="col-sm-2 col-form-label">Banco</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="database" placeholder="Banco">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="table" class="col-sm-2 col-form-label">Tabela</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="table" placeholder="Tabela">
+                </div>
+            </div>
+            <div class="form-group row">
+                <div class="col-sm-10">
+                    <button type="submit" class="btn btn-success">Conectar</button>
                 </div>
             </div>
         </form>
