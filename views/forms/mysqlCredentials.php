4fc7909e9d343d0e18d3d4d4868af78b30cb5acf
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 16:46:39 -0300

    Fix name of inputs

--- views/forms/mysqlCredentials.php
+++ views/forms/mysqlCredentials.php
@@ -1,39 +1,40 @@
 <div class="row">
     <div class="col-md-8 order-md-1">
         <h4 class="mb-3">Credenciais</h4>
-        <form action="" method="POST" class="needs-validation" novalidate autocomplete="off">
+        <form action="/database/tableInfo" method="POST" class="needs-validation" novalidate autocomplete="off">
             <div class="form-group row">
                 <label for="host" class="col-sm-2 col-form-label">Host</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="host" placeholder="Ex.: 192.168.0.0">
+                    <input type="text" class="form-control" id="host" name="host" placeholder="Ex.: 192.168.0.0">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="usuario" class="col-sm-2 col-form-label">Usuário</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="usuario" placeholder="Usuário">
+                    <input type="text" class="form-control" id="usuario" name="user" placeholder="Usuário">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="password" class="col-sm-2 col-form-label">Senha</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="password" placeholder="Senha">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Senha">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="database" class="col-sm-2 col-form-label">Banco</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="database" placeholder="Banco">
+                    <input type="text" class="form-control" id="database" name="database" placeholder="Banco">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="table" class="col-sm-2 col-form-label">Tabela</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="table" placeholder="Tabela">
+                    <input type="text" class="form-control" id="table" name="table" placeholder="Tabela">
                 </div>
             </div>
             <div class="form-group row">
                 <div class="col-sm-10">
+                    <input type="hidden" name='db_type' value="<?=$_GET['db_type'] ?? '' ?>" />
                     <button type="submit" class="btn btn-success">Conectar</button>
                 </div>
             </div>
