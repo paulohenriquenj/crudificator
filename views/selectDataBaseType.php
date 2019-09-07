bbf6b7156975b4c7a729117cb63e59ff92af1c24
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 14:53:37 -0300

    Adjusting layout

--- views/selectDataBaseType.php
+++ views/selectDataBaseType.php
@@ -1,8 +1,19 @@
-<center class="selecione">
-    <h2>Selecione o tipo da base de dados</h2>
-    
-    
-</center>
+<div style="margin-left: 10%">
+    <center class="selecione">
+        <h2>Selecione o tipo da base de dados</h2>
+            <form action="/config/database" method="GET" class="form-inline">
+                <select name="db_type" class="custom-select" style="width:50%; margin: 30px;">
+                    <option value=''>--</option>
+                    <?php
+                        foreach ($supportedDataBases['supported_databases'] as $dataBase) {
+                            echo '<option value="'.$dataBase.'">'.ucfirst($dataBase).'</option>';
+                        }
+                    ?>
+            </select>
+            <button type="submit" class="btn btn-success">Success</button>
+        </form>
+    </center>
+</div>
 
 <style>
     .selecione{
