d48f57aef8ff1216cfe2610220435a22eaa34b1f
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 20:48:59 -0300

    Adjust layout

--- src/html/partials/form.php
+++ src/html/partials/form.php
@@ -1,7 +1,7 @@
 <?php
 
 return '
-    <form action="__action" method="__method__" class="__class__">
-        __input__
+    <form action="__action__" method="__method__" class="__class__">
+        __inputs__
     </form>
-'
+';
