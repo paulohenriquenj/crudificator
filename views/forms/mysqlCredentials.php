bc16a510460b593402b2be304ddfb1df36efedb8
Simonarde Lima <simonarde@gmail.com>
2019-09-07 15:07:35 -0300

    add host and database, fixing typos

--- views/forms/mysqlCredentials.php
+++ views/forms/mysqlCredentials.php
@@ -21,9 +21,36 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text"><i class="fa fa-fw fa-eye-slash"></i></span>
                         </div>
-                        <input type="password" class="form-control" id="password" placeholder="Usuário" required style="border-radius: 3px">
+                        <input type="password" class="form-control" id="password" placeholder="Senha" required style="border-radius: 3px">
                         <div class="invalid-feedback" style="width: 100%;">
-                            O Usuário é obrigatório
+                            A senha é obrigatória
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label for="host">Host</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-fw fa-link"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="host" placeholder="Host" required style="border-radius: 3px">
+                        <div class="invalid-feedback" style="width: 100%;">
+                            O Host é obrigatório
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label for="database">Banco</label>
+                    <div class="input-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fa fa-fw fa-server"></i></span>
+                        </div>
+                        <input type="text" class="form-control" id="database" placeholder="Banco" required style="border-radius: 3px">
+                        <div class="invalid-feedback" style="width: 100%;">
+                            O banco é obrigatório
                         </div>
                     </div>
                 </div>
