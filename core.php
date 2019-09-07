832f4d6bcbbda9f2f43b41e4b09cae5be6d953c4
Paulo Jesus <paulohenrique.nj@gmail.com>
2019-09-07 15:45:21 -0300

    Add utils

--- core.php
+++ core.php
@@ -1,6 +1,8 @@
 <?php
 
-require __DIR__.'/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
+
+require_once __DIR__.'/src/utils.php';
 
 $supportedDataBases = require 'src/supportedDatabases.php';
 
