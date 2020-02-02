b70e6cd1dc983b579334e41d0bbf020c9c9b5beb
Simonarde Lima <simonarde@gmail.com>
2020-02-02 15:10:42 -0300

    Adicionando dotenv

--- core.php
+++ core.php
@@ -4,5 +4,10 @@ require_once __DIR__.'/vendor/autoload.php';
 
 require_once __DIR__.'/src/utils.php';
 
+if( file_exists(__DIR__ . '/.env') ) {
+    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+    $dotenv->load();
+}
+
 $supportedDataBases = require 'src/supportedDatabases.php';
 
--- views/forms/mysqlCredentials.php
+++ views/forms/mysqlCredentials.php
@@ -5,31 +5,31 @@
             <div class="form-group row">
                 <label for="host" class="col-sm-2 col-form-label">Host</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="host" name="host" placeholder="Ex.: 192.168.0.0">
+                    <input type="text" class="form-control" id="host" name="host" placeholder="Ex.: 192.168.0.0" value="<?=getenv("DB_HOST") ?: '';?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="usuario" class="col-sm-2 col-form-label">Usuário</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="usuario" name="user" placeholder="Usuário">
+                    <input type="text" class="form-control" id="usuario" name="user" placeholder="Usuário" value="<?=getenv("DB_USER") ?: '';?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="password" class="col-sm-2 col-form-label">Senha</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="password" name="password" placeholder="Senha">
+                    <input type="password" class="form-control" id="password" name="password" placeholder="Senha" value="<?=getenv("DB_PASS") ?: '';?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="database" class="col-sm-2 col-form-label">Banco</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="database" name="database" placeholder="Banco">
+                    <input type="text" class="form-control" id="database" name="database" placeholder="Banco" value="<?=getenv("DB_DATABASE") ?: '';?>">
                 </div>
             </div>
             <div class="form-group row">
                 <label for="table" class="col-sm-2 col-form-label">Tabela</label>
                 <div class="col-sm-10">
-                    <input type="text" class="form-control" id="table" name="table" placeholder="Tabela">
+                    <input type="text" class="form-control" id="table" name="table" placeholder="Tabela" value="">
                 </div>
             </div>
             <div class="form-group row">
